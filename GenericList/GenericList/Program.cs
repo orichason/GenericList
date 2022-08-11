ff6a7b72267bdffda6c6dba1d90a8ef0b5207544
orichason <orichason@gmail.com>
2022-08-11 13:58:19 -0700

    test

--- GenericList/GenericList/Program.cs
+++ GenericList/GenericList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericList
 {
@@ -6,7 +7,8 @@ namespace GenericList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");ssss
+            Console.WriteLine("Hello World!");
+            l
         }
     }
 }
